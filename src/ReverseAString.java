7919339c499848eca6848c34ab7b734581cf4eb2
Aditya Kappagantula <kappagantulaaditya@gmail.com>
2015-03-23 18:48:03 -0700

    Added the method to reverse a string

---
+++ src/ReverseAString.java
@@ -0,0 +1,22 @@
+public class ReverseAString {
+	// Input: Any case-sensitive string
+	// Output: Reversed case-sensitive string
+	public static void main(String[] args) {
+		ReverseAString instance = new ReverseAString();
+		instance.reverse("Aditya");
+	}
+
+	public void reverse(String aString) {
+		char[] letters = new char[aString.length()];
+		for (int i = 0; i < letters.length; i++) {
+			letters[i] = aString.charAt(i);
+		}
+		char temporaryCharacter;
+		for (int i = 0; i < letters.length / 2; i++) {
+			temporaryCharacter = letters[i];
+			letters[i] = letters[letters.length - i - 1];
+			letters[letters.length - i - 1] = temporaryCharacter;
+		}
+		System.out.println(new String(letters));
+	}
+}
